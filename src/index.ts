3b485af5ab805ad2229a8e7eebfb141953929169
hariyogi <hariyogi.office@gmail.com>
2022-10-26 00:16:41 +0800

    Adding set player name input

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import LeftSide from "./view/LeftSide.view";
 import { MainBody, replaceBody } from "./view/MainBody.view";
-import { ChosseGameMethod } from "./view/StartGame.view";
+import { chosseGameMethod, chossePlayerName } from "./view/StartGame.view";
 import "./styles/style.css"
 
 const body = document.body;
@@ -8,8 +8,19 @@ const body = document.body;
 body.appendChild(LeftSide());
 body.appendChild(MainBody());
 
-replaceBody(ChosseGameMethod(method => {
-    console.log(method);
-}));
+function setGameMethod() {
+    replaceBody(chosseGameMethod(method => chosePlayerNameEll(method)));
+}
+
+function chosePlayerNameEll(method: "PLAYER" | "BOT") {
+    replaceBody(chossePlayerName(method, (p1: string, p2: string) => {
+        console.log(p1);
+        console.log(p2)
+    }, () => setGameMethod()));
+}
+
+setGameMethod();
+
+
 
 
--- src/view/StartGame.view.ts
+++ src/view/StartGame.view.ts
@@ -7,9 +7,33 @@ function createMethodButton(content: string, id: string, onClick: () => void) {
     return methodButton;
 }
 
-export const ChosseGameMethod = (
+function createNameInput(label: string, id: string, onChange: (value: string) => void) {
+    const containerEll = document.createElement("div");
+    containerEll.classList.add("name-input-cont");
+
+    const labelEll = document.createElement("label");
+    labelEll.textContent = label;
+    labelEll.htmlFor = id;
+    containerEll.appendChild(labelEll);
+
+    const input = document.createElement("input");
+    input.id = id;
+    input.name = id;
+    input.type = "text";
+    input.placeholder = "Masukan nama";
+    input.addEventListener("change", (e) => {
+        const target = e.target as HTMLInputElement;
+        onChange(target.value);
+    })
+
+    containerEll.appendChild(input);
+
+    return containerEll;
+}
+
+export function chosseGameMethod (
     callBack: (method: "PLAYER" | "BOT") => void
-) => {
+) {
     const title = document.createElement("h2");
     title.textContent = "Pilih mode permainan";
     title.classList.add("title");
@@ -39,9 +63,52 @@ export const ChosseGameMethod = (
     return div;
 }
 
-// export function chossePlayerName(method: "PLAYER" | "BOT") {
-//     if(method === "PLAYER") {
+export function chossePlayerName(
+    method: "PLAYER" | "BOT",
+    callBack: (player1: string, player2: string) => void,
+    onBack: () => void
+) {
+    let player1Name = "Player 1";
+    let player2Name = "Player 2";
 
-//     }
-// }
+    const container = document.createElement("div");
+    container.classList.add("insert-name");
+
+    const title = document.createElement("h2");
+    title.textContent = "Masukan nama pemain...";
+    container.appendChild(title);
+    
+    const player1 = createNameInput(
+        "Player 1", 
+        "player-1", 
+        (c) => player1Name = c
+    );
+
+    const player2 = createNameInput(
+        "Player 2", 
+        "player-2",
+        (c) => player2Name = c
+    );
+
+    container.appendChild(player1);
+
+    if(method === "PLAYER") {
+        container.appendChild(player2);
+    }
+
+    const nextButton = document.createElement("button");
+    nextButton.addEventListener("click", () => callBack(player1Name, player2Name));
+    nextButton.textContent = "Lanjut";
+    nextButton.type = "button";
+
+    const backButton = document.createElement("button");
+    backButton.addEventListener("click", () => onBack());
+    backButton.classList.add("back-btn");
+    backButton.textContent = "Kembali";
+    backButton.type = "button";
+
+    container.appendChild(nextButton);
+    container.appendChild(backButton);
+    return container;
+}
 
